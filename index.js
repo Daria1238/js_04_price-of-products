5adc9601eb7e25390c432b8eaf54b00e3c5f3c5b
daria <daria.ruchkina4@gmail.com>
2022-11-01 20:25:03 +0200

    homework4

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function priceOfProducts(saltBeefPrice, saltBeefRequest, riceRequest) {
-	// write your code here
+	const totalBeefPrice = saltBeefPrice * saltBeefRequest;
+	const ricePrice = Math.round(saltBeefPrice / 3 * 100) / 100;
+	const totalRicePrice = ricePrice * riceRequest;
+	const totalPrice = totalBeefPrice + totalRicePrice;
+
+	return totalPrice;
 }
 
 module.exports = priceOfProducts;
